e376722f750cbd022a8411830fe9ed6245c0abbf
Mohak <mohakagr@gmail.com>
2016-01-28 12:37:33 +0530

    Stopwatch with pause,play and stop controls implemented

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var timer = NSTimer()
+    var timerValue = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setTimer() {
+        timerValue++
+        timerLabel.text = String(timerValue)
+    }
+    
+    @IBAction func playCallback(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("setTimer"), userInfo: nil, repeats: true)
+    }
 
-
+    @IBAction func pauseCallback(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func stopCallback(sender: AnyObject) {
+        timer.invalidate()
+        timerValue = 0
+        timerLabel.text = "0"
+    }
 }
 
